1fde62f6b22c47c9c5fb1c91510889a41de181e7
Zachary Klein <zak@silver-chalice.com>
2017-05-25 23:45:17 -0500

    New home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, Jumbotron } from 'react-bootstrap';
 import AppNav from './AppNav';
 
-import grailsLogo from './images/grails-cupsonly-logo-white.svg';
-import reactLogo from './images/logo.svg';
 import { SERVER_URL, CLIENT_VERSION, REACT_VERSION } from './config';
 import 'whatwg-fetch';
 
@@ -36,32 +34,16 @@ class App extends Component {
     return (
       <div>
         <AppNav serverInfo={serverInfo} clientInfo={clientInfo}/>
-        <div className="grails-logo-container">
-          <img className="grails-logo" src={grailsLogo} alt="Grails" />
-          <span className="plus-logo">+</span>
-          <img className="hero-logo" src={reactLogo} alt="React" />
-        </div>
+
 
         <Grid>
-          <div id="content">
-            <section className="row colset-2-its">
-              <h1 style={{textAlign: 'center'}}>Welcome to Grails</h1>
+          <div>
+            <Jumbotron>
+              <h1 style={{textAlign: 'center'}}>Welcome to the Book Store!</h1>
               <br/>
-              <p>
-                Congratulations, you have successfully started your first Grails + React application! While in development mode, changes will be loaded automatically when you edit your React app, without even refreshing the page.
-                Below is a list of controllers that are currently deployed in
-                this application, click on each to execute its default action:
-              </p>
+            </Jumbotron>
 
-              <div id="controllers" role="navigation">
-                <h2>Available Controllers:</h2>
-                <ul>
-                  {serverInfo.controllers ? serverInfo.controllers.map(controller => {
-                    return <li key={controller.name}><a href={SERVER_URL + controller.logicalPropertyName}>{ controller.name }</a></li>;
-                  }) : null }
-                </ul>
-              </div>
-            </section>
+
 
           </div>
         </Grid>
